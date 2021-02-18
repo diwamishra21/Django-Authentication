f4e052ce3fa8d125ea837ace9d3383ecb40e4c9a
Diwaker <diwamishra21@gmail.com>
2021-02-18 21:52:37 +0530

    Initial Commit

---
+++ DjangoAuthentication/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for DjangoAuthentication project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoAuthentication.settings')
+
+application = get_asgi_application()
---
+++ home/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login
+
+# Username and password for test user is-\
+# user- diwaker
+# pass- Diwa@Mishra
+# Create your views here.
+def index(request):
+    print(request.user)
+    if request.user.is_anonymous:
+        return redirect("/login") 
+    return render(request, 'index.html')
+
+def loginUser(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+
+        # check if user has entered correct credentials
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect("/")
+
+        else:
+            # No backend authenticated the credentials
+            return render(request, 'login.html')
+
+    return render(request, 'login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
